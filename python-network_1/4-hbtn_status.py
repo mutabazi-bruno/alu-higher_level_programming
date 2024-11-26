f50ecc85cc7ccea80c310a04d19ec8e677da3e8d
mutabazi-bruno <b.mutabazi@alustudent.com>
2024-11-26 06:19:22 +0000

    python-network1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Module 4-hbtn_status.py
-"""
-
+"""Documented now"""
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
