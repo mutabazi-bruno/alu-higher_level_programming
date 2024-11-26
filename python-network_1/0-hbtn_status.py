3831ca9a6aec6748a707756df4864fcee02fef77
mutabazi-bruno <b.mutabazi@alustudent.com>
2024-11-26 06:31:32 +0000

    python-network1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
+# Update the URL as required by the test
+url = 'http://0.0.0.0:5050/status'
+
+# Define the headers
 headers = {
     'User-Agent': (
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
@@ -9,11 +13,15 @@ headers = {
     )
 }
 
+# Make the request
 req = urllib.request.Request(url, headers=headers)
+
+# Use the `with` statement to handle the request and response
 with urllib.request.urlopen(req) as response:
     content = response.read()
     print("Body response:")
     print("\t- type:", type(content))
     print("\t- content:", content)
     print("\t- utf8 content:", content.decode("utf-8"))
+i
 
