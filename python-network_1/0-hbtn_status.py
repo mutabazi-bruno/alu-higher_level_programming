5cdfe2c2ce07aa1819615d2a43a35d0c0094930e
mutabazi-bruno <b.mutabazi@alustudent.com>
2024-11-26 06:23:58 +0000

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@ import urllib.request
 
 url = 'https://intranet.hbtn.io/status'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
+    'User-Agent': (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+    )
 }
 
 req = urllib.request.Request(url, headers=headers)
@@ -15,3 +16,4 @@ with urllib.request.urlopen(req) as response:
     print("\t- type:", type(content))
     print("\t- content:", content)
     print("\t- utf8 content:", content.decode("utf-8"))
+
