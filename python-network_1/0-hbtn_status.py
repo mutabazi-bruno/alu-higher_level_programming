bd3e7e8468fa4103e3172b222baff6f42be0ef4d
mutabazi-bruno <b.mutabazi@alustudent.com>
2024-11-26 09:13:15 +0200

    python-network1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
 import urllib.request
-import os
 
-# Use a default URL, but allow override via environment variable
-url = os.getenv('TEST_URL', 'https://alu-intranet.hbtn.io/status')
+url = 'https://intranet.hbtn.io/status'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
+    '\n    Chrome/99.0.4844.84 Safari/537.36',
+}
 
-# Send the request and handle the response
-with urllib.request.urlopen(url) as response:
+req = urllib.request.Request(url, headers=headers)
+with urllib.request.urlopen(req) as response:
     content = response.read()
     print("Body response:")
     print("\t- type:", type(content))
