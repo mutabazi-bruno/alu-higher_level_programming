7cb00e9f453734f1f5c288b1d89cac716e3e402b
mutabazi-bruno <b.mutabazi@alustudent.com>
2024-11-26 09:27:00 +0200

    python-network1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
 import urllib.request
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = 'https://intranet.hbtn.io/status'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
+    '\n    Chrome/99.0.4844.84 Safari/537.36',
+}
 
-
-with urllib.request.urlopen(url) as response:
+req = urllib.request.Request(url, headers=headers)
+with urllib.request.urlopen(req) as response:
     content = response.read()
     print("Body response:")
     print("\t- type:", type(content))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
+"""Documented now"""
 import requests
 
-
-url = 'https://alu-intranet.hbtn.io/status'
-
-
-response = requests.get(url)
-
-
-content = response.text
-
-
-print("Body response:")
-print("\t- type:", type(content))  
-print("\t- content:", content)    
+if __name__ == '__main__':
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
