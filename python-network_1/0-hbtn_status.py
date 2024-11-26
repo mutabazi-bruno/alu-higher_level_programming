20cd605101ffe002e6e4e1f8b010b085b80a4593
mutabazi-bruno <b.mutabazi@alustudent.com>
2024-11-26 09:17:26 +0200

    python-network1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-
 import urllib.request
-
 
 url = 'https://alu-intranet.hbtn.io/status'
 
 
 with urllib.request.urlopen(url) as response:
-    content = response.read()  
+    content = response.read()
     print("Body response:")
-    print("\t- type:", type(content)) 
-    print("\t- content:", content)  
-    print("\t- utf8 content:", content.decode('utf-8')) 
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode("utf-8"))
